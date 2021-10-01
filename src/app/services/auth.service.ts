cf039d284fb95cdf246f3ea2cb44f9f588fae6c7
Diogo Alves <Diogo2560@gmail.com>
2021-09-30 22:18:13 -0300

    Serviço de sessão criado

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,40 +1,51 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { GlassHttpResponse } from '../shared/interfaces/types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  
-  private readonly url = `${environment.url_api}user/login`;
+
+  private readonly url = `${environment.url_api}user/`;
   private readonly tokenFieldName = 'user_token';
+  private headers: HttpHeaders;
 
-  constructor(private http: HttpClient) { }
-
-  public async login(username: string, password: string) {
-    const body = {
-      username: 'diogo2550',
-      password: '12345'
-    };
-    const observable = await this.http.post(this.url, body).toPromise();
-
-    console.log(observable);
+  constructor(private http: HttpClient) {
+    this.headers = new HttpHeaders({
+      'Content-Type': 'text/plain;charset=UTF-8',
+    });
   }
 
-  public logout() {
+  public async login(cpf: string, password: string): Promise<boolean> {
+    const body = { cpf, password };
+    const response: GlassHttpResponse = await this.http.post<GlassHttpResponse>(`${this.url}login`, body, {
+      	headers: this.headers
+    }).toPromise();
 
+    if(response.success) {
+      this.setToken(response.data.token);
+      return true;
+    }
+    return false;
+  }
+
+  /** Usado para saber se o usuário possui uma sessão ativa. A sessão só terminará quando o método logout() for chamado */
+  public logout() {
+    localStorage.removeItem(this.tokenFieldName);
   }
 
   public isLogged() {
-    return localStorage.getItem(this.tokenFieldName) != null;
+    return this.getToken() != null;
   }
 
-  /** Pode ser usado para obter alguma informações de dentro do token😡
+  /** Pode ser usado para obter alguma informações de dentro do token 😡.
+   * Dentro do token você terá as opções: email, name, id
    * @return string|undefined string a chave exista, null caso a chave seja inválida.
    */
   public getTokenData(key: string): string {
-    let token = localStorage.getItem('user_token');
+    let token = this.getToken();
     if(!token) {
       return 'Token inexistente';
     }
@@ -43,6 +54,14 @@ export class AuthService {
     let data = JSON.parse(payload)[key];
 
     return data;
+  }
+
+  private setToken(token: string) {
+    localStorage.setItem(this.tokenFieldName, token);
+  }
+
+  private getToken() {
+    return localStorage.getItem(this.tokenFieldName);
   }
 
 }
--- src/app/shared/interfaces/types.ts
+++ src/app/shared/interfaces/types.ts
@@ -7,6 +7,13 @@ export interface WebsocketResponse {
     data?: any
 }
 
+export interface GlassHttpResponse {
+    code: number,
+    success: boolean,
+    error?: string,
+    data?: any
+}
+
 export interface GetAllRequestBody {
     employeeId: number,
     month: number,
@@ -90,4 +97,3 @@ export interface ActivityListInput {
     appointments: [string, Appointment | null][] | undefined // Se isso nao for um array nao funciona
     date: Date
 }
-  
